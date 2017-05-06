df70af656ed50dc60a0276828860e30b214cd329
shawn cresante <shawn@cresante.com>
2017-05-06 00:25:58 -0400

    remove pipe logic

--- monitor-rotate.sh
+++ monitor-rotate.sh
@@ -7,26 +7,17 @@
 command -v monitor-sensor >/dev/null 2>&1 || { echo >&2 "I require monitor-sensor but it's not installed.  Aborting."; exit 1; }
 command -v xmodmap >/dev/null 2>&1 || { echo >&2 "I require xmodmap but it's not installed.  Aborting."; exit 1; }
 
-# switch between pipe and filemode
-FML='file'
+# switch to cwd
+cd $PWD
 
 #cleanup last run
 LOG=sensor.log
 : > sensor.log
 killall -q -v monitor-sensor
 
-# create FIFO
-PIPE=/var/run/user/$UID/sensorpipe
-[ -e $PIPE ] || mkfifo $PIPE
-
 # Launch monitor-sensor and output
-if [ $FML == 'file' ]; then
-    monitor-sensor >> $LOG &
-    STATE=$LOG
-    elif [ $FML == 'pipe' ]; then
-    monitor-sensor >> $PIPE &
-    STATE=$PIPE
-fi
+monitor-sensor >> $LOG &
+STATE=$LOG
 
 PID=$!
 # kill monitor-sensor if this script exits
@@ -46,13 +37,13 @@ while [ -d /proc/$PID ] ; do
             # Set the actions to be taken for each possible orientation
             case "$ORIENT" in
             normal)
-              /opt/rotate-screen.sh -n;;
+              ./rotate-screen.sh -n;;
             bottom-up)
-              /opt/rotate-screen.sh -u;;
+              ./rotate-screen.sh -u;;
             right-up)
-              /opt/rotate-screen.sh -r;;
+              ./rotate-screen.sh -r;;
             left-up)
-              /opt/rotate-screen.sh -l;;
+              ./rotate-screen.sh -l;;
             esac
         fi
     done
